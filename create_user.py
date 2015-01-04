856a1287a1794ab059d3103d941bc679c4096a39
Benedikt Waldvogel <mail@bwaldvogel.de>
2015-01-04 16:20:21 +0100

    add test for create_user.py

--- create_user.py
+++ create_user.py
@@ -7,26 +7,30 @@ import sys
 from webapp import app_bcrypt
 import xkcdpass.xkcd_password as xp
 
-app = webapp.init()
-app.config.update(SQLALCHEMY_ECHO=False)
 
-if len(sys.argv) != 2:
-    print("usage: %s <username>" % sys.argv[0])
-    sys.exit(1)
+def create_user(app, username):
+    with app.app_context():
+        wordfile = xp.locate_wordfile()
+        mywords = xp.generate_wordlist(wordfile=wordfile, min_length=5, max_length=8)
+        password = xp.generate_xkcdpassword(mywords, acrostic="ambit")
 
-username = sys.argv[1]
+        assert not User.query.filter_by(username=username).scalar(), "user already exists"
 
-with app.app_context():
-    wordfile = xp.locate_wordfile()
-    mywords = xp.generate_wordlist(wordfile=wordfile, min_length=5, max_length=8)
-    password = xp.generate_xkcdpassword(mywords, acrostic="ambit")
+        user = User(username=username)
+        user.password = app_bcrypt.generate_password_hash(password, 10)
+        user.active = True
+        db.session.add(user)
+        db.session.commit()
 
-    assert not User.query.filter_by(username=username).scalar(), "user already exists"
+        print("created user '%s'" % user.username)
+        print("generated password: '%s'" % password)
 
-    user = User(username=username)
-    user.password = app_bcrypt.generate_password_hash(password, 10)
-    user.active = True
-    db.session.add(user)
-    db.session.commit()
-    print("created user '%s'" % user.username)
-    print("generated password: '%s'" % password)
+
+if __name__ == "__main__":
+    app = webapp.init()
+    app.config.update(SQLALCHEMY_ECHO=False)
+    if len(sys.argv) != 2:
+        print("usage: %s <username>" % sys.argv[0])
+        sys.exit(1)
+
+    create_user(app=app, username=sys.argv[1])
--- tests/test_webapp.py
+++ tests/test_webapp.py
@@ -3,6 +3,9 @@
 import webapp
 from create_schema import create_schema
 from recreate_schema import recreate_schema
+from create_user import create_user
+from model import User
+import pytest
 
 app = None
 
@@ -14,10 +17,26 @@ class TestWebapp(object):
         global app
         app = webapp.init()
         db_uri = 'sqlite:///:memory:'
-        app.config.update(SQLALCHEMY_ECHO=False, TESTING=True, SQLALCHEMY_DATABASE_URI=db_uri)
+        app.config.update(SQLALCHEMY_ECHO=False, DEBUG=True, TESTING=True, SQLALCHEMY_DATABASE_URI=db_uri)
 
     def test_create_schema(self):
         create_schema(app)
 
     def test_recreate_schema(self):
         recreate_schema(app)
+
+    def test_create_user(self):
+        with app.test_request_context():
+            assert User.query.count() == 0
+        create_user(app, username='test_user')
+        with app.test_request_context():
+            assert User.query.count() == 1
+            assert User.query.filter_by(username='test_user').one()
+
+        with pytest.raises(AssertionError):
+            create_user(app, username='test_user')
+
+        create_user(app, username='test_user2')
+        with app.test_request_context():
+            assert User.query.count() == 2
+            assert User.query.filter_by(username='test_user').one() != User.query.filter_by(username='test_user2').one()
