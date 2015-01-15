782a953676c9523cab83492773748beb933ed78a
Benedikt Waldvogel <mail@bwaldvogel.de>
2015-01-15 21:03:45 +0100

    add unit tests

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -2,6 +2,7 @@
 
 import filters
 from datetime import timedelta
+import pytest
 
 
 class TestFilters(object):
@@ -12,3 +13,14 @@ class TestFilters(object):
         assert filters.duration(127.12) == "00:02:07.12"
         assert filters.duration(127) == "00:02:07.00"
         assert filters.duration(timedelta(seconds=127)) == "00:02:07.00"
+
+    def test_swimPace(self):
+        assert filters.swimPace(timedelta(seconds=12.3456)) == "20:34.56 min / 100 m"
+
+    def test_swimPace_illegalValue(self):
+        with pytest.raises(AssertionError):
+            filters.swimPace(1)
+
+    def test_swimPace_valueTooLarge(self):
+        with pytest.raises(AssertionError):
+            filters.swimPace(timedelta(seconds=999))
