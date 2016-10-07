00c456c21d0ae6abf799b965dbb5a829cee3b829
Benedikt Waldvogel <mail@bwaldvogel.de>
2016-10-07 16:43:01 +0200

    use FlaskForm instead of Form (deprecated)

--- login.py
+++ login.py
@@ -2,7 +2,7 @@
 # vim: set fileencoding=utf-8 :
 
 from flask_login import LoginManager
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 
 from model import User
@@ -10,7 +10,7 @@ from model import User
 login_manager = LoginManager()
 
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     username = StringField()
     password = PasswordField()
 
